82fd099d3168d36357496c10475e9cd543d98f87
Youssef M. Khataan <ykhat3an@gmail.com>
2023-11-29 14:28:20 +0200

    added main page client

--- src/main/java/com/example/segaoop/HelloApplication.java
+++ src/main/java/com/example/segaoop/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         stage.setTitle("Hello!");
-        //stage.setScene(scene);
+        //stage.setScene(scene);//
         stage.show();
     }
 
