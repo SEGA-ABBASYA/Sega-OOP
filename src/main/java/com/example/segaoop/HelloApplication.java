f5407d8622a2cacd9eb2d32d662dc21bee955b2f
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-11-30 17:13:14 +0200

    Login Mango

--- src/main/java/com/example/segaoop/HelloApplication.java
+++ src/main/java/com/example/segaoop/HelloApplication.java
@@ -10,8 +10,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        stage.setTitle("Hello!");
+        //stage.setTitle("Hello!");//
         //stage.setScene(scene);//
+        //stage.show();//
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LoginPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
         stage.show();
     }
 
