492abe41b159efbfeb702684ba68649973ea5f28
Youssefproof771 <youssefproof@gmail.com>
2023-11-30 15:20:44 +0200

    EZ added Client View

--- src/main/java/com/example/segaoop/HelloController.java
+++ src/main/java/com/example/segaoop/HelloController.java
@@ -1,14 +1,130 @@
 package com.example.segaoop;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.*;
+
+public class HelloController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TableColumn<Client, Integer> AccountNumberColumn;
+
+    @FXML
+    private TableView<Client> ClientsTable;
+
+    @FXML
+    private Button CreateButton;
+
+    @FXML
+    private Button DeleteButton;
+
+    @FXML
+    private Button EditButton;
+
+    @FXML
+    private TableColumn<Client, String> FirstNameColumn;
+
+    @FXML
+    private TableColumn<Client, Integer> IDColumn;
+
+    @FXML
+    private TableColumn<Client, String> LastNameColumn;
+
+    @FXML
+    private Button SearchButton;
+
+    @FXML
+    private TextField SearchTextField;
+
+    @FXML
+    private TableColumn<Client, String> StateColumn;
+
+    @FXML
+    private TableColumn<Client,Float> BalanceColumn;
+
+    @FXML
+    private TableColumn<Client, Integer> TelephoneNumberColumn;
+
+    @FXML
+    private TableColumn<Client, String> TypeColumn;
+
+    @FXML
+    private TableColumn<Client, String> UsernameColumn;
+
+    @FXML
+    void BeginSearch(MouseEvent event) {
+        ObservableList<Client> tobeaddedlist = FXCollections.observableArrayList();
+        for (Client item:ClientTestList) {
+            String s = Integer.toString(item.getAccountNumber()).toLowerCase();
+            if (s.contains(SearchTextField.getText().toLowerCase()))
+            {
+                tobeaddedlist.add(item);
+            }
+        }
+        ClientsTable.setItems(tobeaddedlist);
+    }
+
+    @FXML
+    void GoToCreateScene(MouseEvent event) {
+
+    }
+
+    @FXML
+    void GoToEditScene(MouseEvent event) {
+
+    }
+
+    ObservableList<Client> ClientTestList = FXCollections.observableArrayList(
+            new Client(15000.0F,1,123,"Youssef","Ashraf",
+                    "Youssefproof","Youssef2004","Savings","Active",01066555),
+            new Client(50000.0F,2,124,"Youssef","Ahmed",
+                    "Herofis","HAHS1234","Savings","Active",10241224)
+    );
+
+
+    @FXML
+    void RemoveSelectedItem(MouseEvent event) {
+        if (!ClientsTable.getSelectionModel().getSelectedItems().isEmpty())
+        {
+            int selectedIndex = ClientsTable.getSelectionModel().getSelectedIndex();
+            Integer selectedAccountNumber = ClientsTable.getSelectionModel().getSelectedItem().getAccountNumber();
+            for (Client item : ClientTestList
+                 ) {
+                if (Objects.equals(item.getAccountNumber(), selectedAccountNumber))
+                {
+                    ClientTestList.remove(item);
+                    break;
+                }
+            }
+            ClientsTable.getItems().remove(selectedIndex);
+
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        IDColumn.setCellValueFactory(new PropertyValueFactory<Client,Integer>("ID"));
+        FirstNameColumn.setCellValueFactory(new PropertyValueFactory<Client,String>("FirstName"));
+        LastNameColumn.setCellValueFactory(new PropertyValueFactory<Client,String>("LastName"));
+        TypeColumn.setCellValueFactory(new PropertyValueFactory<Client,String>("Type"));
+        StateColumn.setCellValueFactory(new PropertyValueFactory<Client,String>("State"));
+        AccountNumberColumn.setCellValueFactory(new PropertyValueFactory<Client,Integer>("AccountNumber"));
+        TelephoneNumberColumn.setCellValueFactory(new PropertyValueFactory<Client,Integer>("TelephoneNumber"));
+        BalanceColumn.setCellValueFactory(new PropertyValueFactory<Client,Float>("Balance"));
+        UsernameColumn.setCellValueFactory(new PropertyValueFactory<Client,String>("Username"));
+
+        ClientsTable.setItems(ClientTestList);
     }
 }
+
