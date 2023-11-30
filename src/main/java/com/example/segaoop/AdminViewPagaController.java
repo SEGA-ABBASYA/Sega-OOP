8651112b4f688afbde50953757d4d8237f448b08
David Magdy <divoMagdy@gmail.com>
2023-11-30 22:13:22 +0200

    Redesigned the Employee paga for the admin and the account page for the admin too

---
+++ src/main/java/com/example/segaoop/AdminViewPagaController.java
@@ -0,0 +1,4 @@
+package com.example.segaoop;
+
+public class AdminViewPagaController {
+}
--- src/main/java/com/example/segaoop/HelloApplication.java
+++ src/main/java/com/example/segaoop/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.segaoop;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,8 +11,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        stage.setTitle("Hello!");
-        //stage.setScene(scene);
+        Parent root = FXMLLoader.load(getClass().getResource("AdminViewPage.fxml"));
+        stage.setTitle("Sega Bank");
+        stage.setScene(new Scene(root,1280,720));
         stage.show();
     }
 
