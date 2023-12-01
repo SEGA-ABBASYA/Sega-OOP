5adbe3a26ae5713ce5c1cedf70a82f7ed03786ea
Youssef M. Khataan <ykhat3an@gmail.com>
2023-12-01 11:35:52 +0200

    updated client main page

--- src/main/java/com/example/segaoop/HelloApplication.java
+++ src/main/java/com/example/segaoop/HelloApplication.java
@@ -1,7 +1,9 @@
 package com.example.segaoop;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,9 +12,17 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        stage.setTitle("Hello!");
-        //stage.setScene(scene);//
-        stage.show();
+        try
+        {
+            Parent root = FXMLLoader.load(getClass().getResource("MainPageClientController.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
