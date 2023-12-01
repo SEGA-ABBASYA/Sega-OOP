4fa8bfb7291a076add0e870f48456295d8573124
David Magdy <divoMagdy@gmail.com>
2023-12-01 15:31:27 +0200

    Final Design For The Admin View

--- src/main/java/com/example/segaoop/AdminViewPagaController.java
+++ src/main/java/com/example/segaoop/AdminViewPagaController.java
@@ -1,4 +1,64 @@
 package com.example.segaoop;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.TreeTableView;
+
 public class AdminViewPagaController {
+
+    @FXML
+    private TreeTableView<?> clientList;
+
+    @FXML
+    private Tab clientTab;
+
+    @FXML
+    private TreeTableView<?> employeeList;
+
+    @FXML
+    private Tab employeeTab;
+
+    @FXML
+    private RadioButton firstNameRadio;
+
+    @FXML
+    private RadioButton firstNameRadioClient;
+
+    @FXML
+    private RadioButton idRadio;
+
+    @FXML
+    private RadioButton idRadioClients;
+
+    @FXML
+    private RadioButton lastNameRadio;
+
+    @FXML
+    private RadioButton salaryRadio;
+
+    @FXML
+    private TextField searchBar;
+
+    @FXML
+    private TextField searchBarClients;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private Button searchButtonClients;
+
+    @FXML
+    private ToggleGroup searchBy;
+
+    @FXML
+    private ToggleGroup searchByClient;
+
+    @FXML
+    private RadioButton usernameRadioClient;
+
 }
