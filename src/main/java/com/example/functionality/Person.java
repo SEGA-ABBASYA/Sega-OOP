fa4bd0b991225bfe23db435e85b9803689d00859
Youssef M. Khataan <ykhat3an@gmail.com>
2023-12-02 12:39:36 +0200

    made person class and functionality package

---
+++ src/main/java/com/example/functionality/Person.java
@@ -0,0 +1,10 @@
+package com.example.functionality;
+import com.example.segaoop.*;
+public abstract class Person
+{
+    protected String id;
+    protected String firstName;
+    protected String lastName;
+    protected String telephone;
+    protected String address;
+}
