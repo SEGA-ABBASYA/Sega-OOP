7295b9c27d4ef9bac0ec2cb0c70a7fb90cd0908f
Youssef M. Khataan <ykhat3an@gmail.com>
2023-12-02 18:28:33 +0200

    added database functionality

--- src/main/java/com/example/functionality/DataBase.java
+++ src/main/java/com/example/functionality/DataBase.java
@@ -1,15 +1,21 @@
 package com.example.functionality;
 import com.example.segaoop.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class DataBase {
     private static DataBase instance;
-    HashMap<String, Person> persons;
+
+    HashMap<String, Person> clients;
+    HashMap<String, Person> employees;
+    ArrayList<Transaction> transactionHistory;
+
     private DataBase() {
 
     }
 
+    // singleton
     public static DataBase getInstance()
     {
         if(instance == null)
@@ -17,5 +23,38 @@ public class DataBase {
             instance = new DataBase();
         }
         return instance;
+    }
+
+    public HashMap<String, Person> getClients() {
+        return clients;
+    }
+    public HashMap<String, Person> getEmployees() {
+        return employees;
+    }
+    public ArrayList<Transaction> getTransactionHistory() {
+        return transactionHistory;
+    }
+
+    public void addClient(String id, Person client)
+    {
+        clients.put(id, client);
+    }
+    public void removeClient(String id)
+    {
+        clients.remove(id);
+    }
+
+    public void addEmployee(String id, Person employee)
+    {
+        employees.put(id, employee);
+    }
+    public void removeEmployee(String id)
+    {
+        employees.remove(id);
+    }
+
+    public void addTransaction(Transaction transaction)
+    {
+        transactionHistory.add(transaction);
     }
 }
