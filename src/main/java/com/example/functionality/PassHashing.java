ac41a6ea6dc1ab70b4d02855dee979d583c42b95
Ziad Khaled <ziadshraf100@gmail.com>
2023-12-05 02:47:06 +0200

    Account Class is Done plus two extra classes

---
+++ src/main/java/com/example/functionality/PassHashing.java
@@ -0,0 +1,26 @@
+package com.example.functionality;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class PassHashing {
+    public static String Hash(String pass){
+        try{
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hashed_bytes = md.digest(pass.getBytes());
+            StringBuilder Hashed_Str = new StringBuilder();
+            for (byte hash : hashed_bytes)
+            {
+                Hashed_Str.append(String.format("%02x", hash));
+            }
+            return Hashed_Str.toString();
+
+        }
+        catch(NoSuchAlgorithmException E)
+        {
+            E.printStackTrace();
+            return "\0";
+        }
+    }
+}
