c0ae1477e02ab057869c5f32fe0ee49465f06f2d
Youssef M. Khataan <ykhat3an@gmail.com>
2023-12-17 19:44:41 +0200

    MESSAGES DONE DIVO PLEASE APPROVE

--- src/main/java/com/example/segaoop/NotificationBox.java
+++ src/main/java/com/example/segaoop/NotificationBox.java
@@ -1,5 +1,6 @@
 package com.example.segaoop;
 import com.example.functionality.*;
+import javafx.event.ActionEvent;
 import javafx.fxml.*;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
@@ -7,34 +8,59 @@ import javafx.scene.control.TableView;
 import javafx.scene.layout.Background;
 import javafx.geometry.*;
 import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-
+import javafx.scene.text.TextFlow;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.io.*;
+import java.util.Objects;
 
 public class NotificationBox {
     public NotificationBox() {
     }
 
-    public Pane getNotificationBox(String sender, String content) throws IOException {
-        Pane root = new Pane();
-        root.prefHeight(100);
-        root.prefWidth(300);
-        root.setBackground(new Background(new BackgroundFill(Color.AQUAMARINE, null, null)));
-        root.setBorder(new javafx.scene.layout.Border(
-                new javafx.scene.layout.BorderStroke(
-                        Color.RED, // Border color
-                        javafx.scene.layout.BorderStrokeStyle.SOLID, // Border style
-                        null, // CornerRadii (null means default)
-                        new javafx.scene.layout.BorderWidths(2) // Border width
-                )
-        ));
-        //root.setPadding(new Insets(50));
-        //Pane root = FXMLLoader.load(getClass().getResource("com/example/segaoop/NotificationBox.fxml"));
-        Label senderLabel = new Label("New Notification From " + sender);
-        Text contentText = new Text(content);
-        root.getChildren().addAll(senderLabel, contentText);
-        return root;
+    public HBox getNotificationBox(String content, String urgency) throws IOException {
+
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER_LEFT);
+        hBox.setPadding(new Insets(5 ,5,5,10));
+        hBox.setSpacing(10);
+
+        Text text = new Text(content);
+        Text urgency_text = new Text(urgency + "\n");
+
+        text.setFont(new Font(22));
+        urgency_text.setFont(new Font(22));
+
+        if(Objects.equals(urgency, "Important"))
+            urgency_text.setFill(Color.ORANGE);
+        else if (Objects.equals(urgency, "Warning"))
+            urgency_text.setFill(Color.RED);
+        else
+            urgency_text.setText("");
+
+        TextFlow textFlow = new TextFlow(urgency_text, text);
+
+        textFlow.setStyle("-fx-color : rgb(239,242,255); " +
+                "-fx-background-color: rgb(15,124,242);" +
+                "-fx-background-radius: 20px;");
+
+        textFlow.setPadding(new Insets(5,10,5,10));
+        text.setFill(Color.color(0.934,0.945,0.996));
+
+        hBox.getChildren().add(textFlow);
+
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateTime = currentDateTime.format(formatter);
+        Label dt = new Label();
+        dt.setText(formattedDateTime);
+        dt.setTextFill(Color.WHITE);
+
+        hBox.getChildren().add(dt);
+        return hBox;
     }
 }
